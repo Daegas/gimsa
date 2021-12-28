2330ed1d5e65e285285eef44b99fe86d1a5d1846
Daegas <dara_224@hotmail.com>
2021-12-28 10:49:59 -0600

    real try commit

---
+++ js/personalized-main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+        $('#content').load('views/index.html');
+
+        
+        // alert('page');
+        // $('#header_menu li a').click(function(){
+        // var page= $(this).attr('href');
+        // $('#content')
+        // alert(page);
+        // })
+})
