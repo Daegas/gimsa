ac22771360314ae607f4f90f16e8c27848757cfc
Daegas <dara_224@hotmail.com>
2022-01-28 15:40:21 -0600

    Catalogo muebles de baño e incio de product detail

---
+++ js/catalogos/mueblesdebano.js
@@ -0,0 +1,56 @@
+// VARIABLES
+var sizes = ['xs', 's', 'l', 'xl']
+var material = ['mar', 'oni']
+var juego = ['eco', 'com']
+
+
+$( document ).ready(function() {
+    //////////////////// START FILTERS //////////////////////
+    // size
+    $('#xs').on('change', function() {
+        Filter(sizes)
+    });
+    $('#s').on('change', function() {
+        Filter(sizes)
+    });
+    $('#l').on('change', function() {
+        Filter(sizes)
+    });
+    $('#xl').on('change', function() {
+        Filter(sizes)
+    });
+    // material
+    $('#mar').on('change', function() {
+        Filter(material)
+    });
+    $('#oni').on('change', function() {
+        Filter(material)
+    });
+    // juegos
+    $('#eco').on('change', function() {
+        Filter(juego)
+    });
+    $('#com').on('change', function() {
+        Filter(juego)
+    });
+    //////////////////// END FILTERS //////////////////////
+
+});
+
+function Filter(array) {
+    for(item of array){
+        HideOrShowClass(item, 'none')
+    }
+    for(item of array){
+        let val = $("#" + item).is(':checked')
+        if(val) HideOrShowClass(item, 'block')
+    }
+}
+
+function HideOrShowClass(classs, hide) {
+    let elements = document.getElementsByClassName(classs)
+    for(item of elements){
+        item.style.display = hide;
+    };
+    
+}
